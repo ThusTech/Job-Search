9c1eefb381d9f953c67628b5566ced43be396036
Thulani Malaza <thmalaz022@student.wethinkcode.co.za>
2024-10-25 12:27:11 +0200

    styling homePage

--- src/components/CardExplorer.jsx
+++ src/components/CardExplorer.jsx
@@ -2,10 +2,22 @@
 
 export default function CardExplorer(props){
     return (
-        <>
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
-            <a href="*">{props.linkDescription}</a>
-        </>
+        <section className="py-4">
+            <div className="container-xl lg:container m-auto">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+                    <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
+                        <h2 className="text-2xl font-bold">
+                            {props.title}
+                        </h2>
+                        <p className="mt-2 mb-4">
+                            {props.description}
+                        </p>
+                        <a className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700" href="*">
+                            {props.linkDescription}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,37 @@
-import logo from '../assets/images/logo.jpg'
+import logo from '../assets/images/logo.png'
 
 
 export default function NavBar(){
     return (
-        <nav>
-            <img src={logo} width="70px"/>
-            <h2>Software Development Jobs</h2>
-            <a href="*">Home</a>
-            <a href="*">Jobs</a>
-            <a href="*">Add Job</a>
+        <nav className='bg-indigo-700 border-b border-indigo-500'>
+            <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+                <div className='flex h-20 items-center justify-between'>
+                    <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
+                        <a className='flex flex-shrink-0 items-center mr-4'>
+                            <img className='h-10 w-auto' src={logo} alt='Jobs'/>
+                            <span className='hidden md:block text-white text-2xl font-bold ml-2'>
+                                Software Development Jobs
+                            </span>
+                        </a>
+
+                        <div className='md:ml-auto'>
+                            <div className='flex space-x-2'>
+                                <a className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' href="*">
+                                    Home
+                                </a>
+
+                                <a className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' href="*">
+                                    Jobs
+                                </a>
+
+                                <a className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' href="*">
+                                    Add Job
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
