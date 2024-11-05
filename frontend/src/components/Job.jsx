9e6d20afa0957379417a3c9a9dc7d93c44ac7bbb
Thulani Malaza <thmalaz022@student.wethinkcode.co.za>
2024-11-05 13:08:08 +0200

    added react icons

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -1,5 +1,6 @@
 import {Link} from 'react-router-dom'
 import {useState} from 'react'
+import {FaMapMarker} from 'react-icons/fa'
 
 
 export default function Job({ job }){
@@ -40,7 +41,7 @@ export default function Job({ job }){
 
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
-                        {/* <FaMapMarker className='inline text-lg mb-1 mr-1' /> */}
+                        <FaMapMarker className='inline text-lg mb-1 mr-1' />
                         {job.location}
                     </div>
 
