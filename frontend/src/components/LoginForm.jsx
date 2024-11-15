6d5e951fa0f9c561f1771c0e765aa246540386f9
Thulani Malaza <thmalaz022@student.wethinkcode.co.za>
2024-11-15 11:03:01 +0200

    fix login error

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,5 +1,6 @@
 import {Link} from 'react-router-dom'
 import { useState } from 'react'
+import loginService from '@/services/LoginService';
 
 export default function LoginForm(){
     const [email, setEmail] = useState('');
